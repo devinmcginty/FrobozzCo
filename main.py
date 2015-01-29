80b3d42844f6e9d48be71d890525e17bea554419
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-01-28 23:35:14 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*- #
+import tweepy
+from auth import *
+
+cKey = CREDENTIALS[C_KEY]
+cSecret = CREDENTIALS[C_SECRET]
+aToken = CREDENTIALS[A_TOKEN]
+aSecret = CREDENTIALS[A_SECRET]
+
+class Listener(tweepy.streaming.StreamListener):
+    def on_data(self, data):
+        print(data)
+        return True
+    def on_error(self, status):
+        print(status)
+
+class ZorkBot(object):
+    def __init__(self):
+        self.api = None
+        self.authenticate()
+    def authenticate(self):
+        auth = tweepy.OAuthHandler(cKey, cSecret)
+        auth.set_access_token(aToken, aSecret)
+        self.api = tweepy.API(auth)
+
+
+if __name__ == '__main__':
+    ZorkBot()
